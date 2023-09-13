7f7886169dec012e55f3e5d3b0a604d57e55ec31
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2023-09-14 00:06:17 +0500

    Readme file added and datetime added

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ import {
   addMenuToggle,
   showContent,
 } from './modules/nav-bar.js';
-
 import { Library } from './modules/library.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
-import {updateTime } from './modules/date.js';
+const updateTime = () => {
+  const time = DateTime.now();
+  const formattedTime = time.toFormat('LLL dd, yyyy, h:mm:ss a');
+  document.getElementById('dateTime').textContent = formattedTime;
+};
 
 // eslint-disable-next-line no-unused-vars
 const library = new Library();
--- modules/date.js
+++
@@ -1,6 +0,0 @@
-import { DateTime } from "luxon";
-export function updateTime() {
-  const time = DateTime.now();
-  const formattedTime = time.toFormat("LLL dd, yyyy, h:mm:ss a");
-  document.getElementById('dateTime').textContent = formattedTime;
-}
